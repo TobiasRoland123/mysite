f92ab7e1b5736ba161645b000b2f72093732081f
Tobias Roland <samu1493@stud.kea.dk>
2024-04-24 07:46:28 +0200

    ...

--- app.py
+++ app.py
@@ -65,6 +65,52 @@ def _():
         return ex
     finally:
         if "db" in locals(): db.close()
+
+
+##############################
+@get("/items/page/<page_number>")
+def _(page_number):
+    try:
+        db = x.db()
+        next_page = int(page_number) + 1
+        offset = (int(page_number) - 1) * x.ITEMS_PER_PAGE
+        q = db.execute(f"""     SELECT * FROM items 
+                                ORDER BY item_created_at 
+                                LIMIT ? OFFSET {offset}
+                        """, (x.ITEMS_PER_PAGE,))
+        items = q.fetchall()
+        print(items)
+
+        is_logged = False
+
+        try:
+            x.validate_user_logged()
+            is_logged =True
+        except:
+            pass
+
+        html = ""
+        for item in items: 
+            html += template("_item", item=item, is_logged=is_logged)
+        btn_more = template("__btn_more", page_number=next_page)
+        if len(items) < x.ITEMS_PER_PAGE: 
+            btn_more = ""
+        return f"""
+        <template mix-target="#items" mix-bottom>
+            {html}
+        </template>
+        <template mix-target="#more" mix-replace>
+            {btn_more}
+        </template>
+        <template mix-function="test">{json.dumps(items)}</template>
+        """
+    except Exception as ex:
+        print(ex)
+        return "ups..."
+    finally:
+        if "db" in locals(): db.close()
+
+
  
 ##############################
 try:
