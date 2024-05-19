f6be6c6b51102e5eae982a88141936e341bb9f66
Tobias Roland Uyet <samu1493@stud.kea.dk>
2024-05-19 23:15:54 +0200

    ...

--- app.js
+++ app.js
@@ -9,13 +9,23 @@ function test(items) {
   });
 }
 
-document.querySelectorAll('.open-dialog-btn').forEach((itemBtn) => {
-  itemBtn.addEventListener('click', function () {
+document.querySelectorAll('.open-dialog-btn').forEach((itemOpenBtn) => {
+  itemOpenBtn.addEventListener('click', function () {
     showDialog(this.getAttribute('data-dialog-id'));
   });
 });
 
+document.querySelectorAll('.close-dialog-btn').forEach((itemCloseBtn) => {
+  itemCloseBtn.addEventListener('click', function () {
+    closeDialog(this.getAttribute('data-dialog-id'));
+  });
+});
 function showDialog(dataDialogId) {
   const dialog = document.querySelector(`#${dataDialogId}`);
   dialog.showModal();
+}
+
+function closeDialog(dataDialogId) {
+  const dialog = document.querySelector(`#${dataDialogId}`);
+  dialog.close();
 }
